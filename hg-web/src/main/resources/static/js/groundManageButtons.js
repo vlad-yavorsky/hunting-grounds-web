29d391dce1374d53053dc5d2fb067b99943b1565
Vladyslav Yavorskyi <vlad.yavorsky.ua@gmail.com>
2020-02-07 05:32:04 +0200

    var to let/const

--- hg-web/src/main/resources/static/js/groundManageButtons.js
+++ hg-web/src/main/resources/static/js/groundManageButtons.js
@@ -1,7 +1,7 @@
 $(function () {
     $('.removeGround').click(function () {
-        var id = $(this).data("ground-id");
-        var name = $(this).data("ground-name");
+        const id = $(this).data("ground-id");
+        const name = $(this).data("ground-name");
         $('.modal-title').text('Remove confirmation');
         $('.modal-body').html('Are you sure want to remove ground <strong>' + name + '</strong>?');
         $('#removeFormGroundId').val(id);
