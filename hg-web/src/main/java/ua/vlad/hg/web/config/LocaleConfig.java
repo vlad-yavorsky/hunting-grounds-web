2be9d76e54b13c3a77dac16f71b6e499ae1fcc10
Vlad Yavorsky <vlad.yavorsky.ua@gmail.com>
2021-10-13 18:24:36 +0300

    disabled cors

--- hg-web/src/main/java/ua/vlad/hg/web/config/LocaleConfig.java
+++ hg-web/src/main/java/ua/vlad/hg/web/config/LocaleConfig.java
@@ -3,6 +3,7 @@ package ua.vlad.hg.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -30,6 +31,11 @@ public class LocaleConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
